1ce5560c2c025306566fdc5fece16e7e87b28fd7
1124ritesh <1124ritesh@gmail.com>
2016-03-03 22:56:43 +0530

    student

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -20,7 +20,8 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body class="sidebar-mini layout-boxed skin-yellow-light">
+<!--skin-yellow-light-->
+<body class="sidebar-mini layout-boxed skin-blue">
 <?php $this->beginBody() ?>
 
 <div class="wrapper">
